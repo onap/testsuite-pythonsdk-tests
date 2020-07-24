dc15463c7ca8ef543a4fd968d215d97c1112f9bf
amine ezziati <aymane.ezziati@gmail.com>
2020-07-24 09:08:42 +0200

    Add CLAMP E2E tests
    Issue-ID: TEST-240
    Signed-off-by: amine ezziati <mohamedamine.ezziati@orange.com>
    Change-Id: Ib612f1670aca10d9ac9f15fadb9cab58a7d8e5a8
    Signed-off-by: mrichomme <morgan.richomme@orange.com>

---
+++ src/onaptests/steps/loop/instantiate_loop.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python
+# http://www.apache.org/licenses/LICENSE-2.0
+"""Instantiation class."""
+import logging
+import logging.config
+
+from onapsdk.clamp.loop_instance import LoopInstance
+from onapsdk.configuration import settings
+
+
+class InstantiateLoop():
+    """class instantiating a closed loop in clamp."""
+
+    def __init__(self, template: str, loop_name: str, operational_policies: list, cert: tuple):
+        self.template=template
+        self.loop_name=loop_name
+        self.operational_policies=operational_policies
+        self.cert=cert
+
+        self._logger: logging.Logger = logging.getLogger("")
+        logging.config.dictConfig(settings.LOG_CONFIG)
+
+    def add_policies(self, loop: LoopInstance) -> None:
+        """add necessary wanted operational policies."""
+        for policy in self.operational_policies:
+            self._logger.info("******** ADD OPERATIONAL POLICY %s *******", policy["name"])
+            added = loop.add_operational_policy(policy_type=policy["policy_type"],
+                                                policy_version=policy["policy_version"])
+            if not added:
+                self._logger.error("an error occured while adding an operational policy")
+            self._logger.info("ADD OPERATION SUCCESSFULY DONE")
+
+
+    def configure_policies(self, loop: LoopInstance) -> None:
+        """Configure all policies."""
+        self._logger.info("******** UPDATE MICROSERVICE POLICY *******")
+        loop._update_loop_details()
+        loop.update_microservice_policy()
+        self._logger.info("******** UPDATE OPERATIONAL POLICIES CONFIG *******")
+        for policy in self.operational_policies:
+            #loop.add_op_policy_config(loop.LoopInstance.__dict__[policy["config_function"]])
+            #possible configurations for the moment
+            if policy["name"] == "MinMax":
+                loop.add_op_policy_config(loop.add_minmax_config)
+            if policy["name"] == "Drools":
+                loop.add_op_policy_config(loop.add_drools_conf)
+            if policy["name"] == "FrequencyLimiter":
+                loop.add_op_policy_config(loop.add_frequency_limiter)
+        self._logger.info("Policies are well configured")
+
+    def deploy(self, loop: LoopInstance) -> None:
+        """Deploy closed loop."""
+        self._logger.info("******** SUBMIT POLICIES TO PE *******")
+        submit = loop.act_on_loop_policy(loop.submit)
+        self._logger.info("******** CHECK POLICIES SUBMITION *******")
+        if submit :
+            self._logger.info("Policies successfully submited to PE")
+
+        else:
+            self._logger.error("An error occured while submitting the loop instance")
+            exit(1)
+        self._logger.info("******** DEPLOY LOOP INSTANCE *******")
+        deploy = loop.deploy_microservice_to_dcae()
+        if deploy:
+            self._logger.info("Loop instance %s successfully deployed on DCAE !!", self.loop_name)
+        else:
+            self._logger.error("An error occured while deploying the loop instance")
+            exit(1)
+
+    def instantiate_loop(self):
+        """Instantiate the control loop."""
+        loop = LoopInstance(template=self.template,
+                            name=self.loop_name,
+                            details={},
+                            cert=self.cert)
+        details = loop.create()
+        if details:
+            self._logger.info("Loop instance %s successfully created !!", self.loop_name)
+        else:
+            self._logger.error("An error occured while creating the loop instance")
+
+        self.add_policies(loop=loop)
+        self.configure_policies(loop=loop)
+        self.deploy(loop=loop)
+
+        loop.details = loop._update_loop_details()
+        return loop
---
+++ src/onaptests/steps/onboard/clamp.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python
+# http://www.apache.org/licenses/LICENSE-2.0
+"""Clamp Onboard service class."""
+from yaml import load
+from onapsdk.sdc.service import Service
+from onapsdk.sdc.vf import Vf
+
+from onapsdk.configuration import settings
+
+from ..base import YamlTemplateBaseStep
+from .service import YamlTemplateVfOnboardStep
+
+class OnboardClampStep(YamlTemplateBaseStep):
+    """Onboard class to create CLAMP templates."""
+
+    def __init__(self, cleanup=False):
+        """Initialize Clamp Onboard object."""
+        super().__init__(cleanup=cleanup)
+        self._yaml_template: dict = None
+        self.add_step(YamlTemplateVfOnboardStep(cleanup=cleanup))
+        # if "service_name" in kwargs:
+        #     self.service_name = kwargs['service_name']
+        # else:
+        #     raise ValueError("Service Name to define")
+        # self.vf_list = []
+        # self.vsp_list = []
+        # self.set_logger()
+
+    @property
+    def yaml_template(self) -> dict:
+        """Step YAML template.
+
+        Load from file if it's a root step, get from parent otherwise.
+
+        Returns:
+            dict: Step YAML template
+
+        """
+        if self.is_root:
+            if not self._yaml_template:
+                with open(settings.SERVICE_YAML_TEMPLATE, "r") as yaml_template:
+                    self._yaml_template: dict = load(yaml_template)
+            return self._yaml_template
+        return self.parent.yaml_template
+
+    @property
+    def service_name(self) -> str:
+        """Service name.
+
+        Get from YAML template if it's a root step, get from parent otherwise.
+
+        Returns:
+            str: Service name
+
+        """
+        if self.is_root:
+            return next(iter(self.yaml_template.keys()))
+        else:
+            return self.parent.service_name
+
+    @YamlTemplateBaseStep.store_state
+    def execute(self):
+        """Onboard service."""
+        super().execute()
+        # retrieve the Vf
+        vf = None
+        for sdc_vf in Vf.get_all():
+            if sdc_vf.name == settings.VF_NAME:
+                vf = sdc_vf
+        self._logger.debug("Vf retrieved %s", vf)
+
+        service: Service = Service(name=self.service_name,
+                                   resources=[vf])
+        service.create()
+        self._logger.info(" Service %s created", service)
+
+        service.add_resource(vf)
+
+        # we add the artifact to the first VNF
+        self._logger.info("Try to add blueprint to %s", vf.name)
+        payload_file = open(settings.CONFIGURATION_PATH + 'clampnode.yaml', 'rb')
+        data = payload_file.read()
+        self._logger.info("DCAE INVENTORY BLUEPRINT file retrieved")
+        service.add_artifact_to_vf(vnf_name=vf.name,
+                                   artifact_type="DCAE_INVENTORY_BLUEPRINT",
+                                   artifact_name="clampnode.yaml",
+                                   artifact=data)
+        payload_file.close()
+        service.checkin()
+        service.onboard()
+        self._logger.info("DCAE INVENTORY BLUEPRINT ADDED")
