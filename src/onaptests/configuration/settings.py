b2455549b2479eab3f62f58a4e351bf3c772d6ce
amine ezziati <aymane.ezziati@gmail.com>
2020-07-24 09:08:42 +0200

    Add CLAMP E2E tests
    Issue-ID: TEST-240
    Signed-off-by: amine ezziati <mohamedamine.ezziati@orange.com>
    Change-Id: Ib612f1670aca10d9ac9f15fadb9cab58a7d8e5a8

--- src/onaptests/configuration/settings.py
+++ src/onaptests/configuration/settings.py
@@ -1,5 +1,5 @@
-"""Speciifc settings module."""  # pylint: disable=bad-whitespace
-
+"""Specific settings module."""  # pylint: disable=bad-whitespace
+import os
 ######################
 #                    #
 # ONAP INPUTS DATAS  #
@@ -21,4 +21,8 @@ TENANT_USER_NAME = "onap-master-daily-vnfs-ci"
 TENANT_USER_PWD = "sZb4hytF7iPSSiq2F3HhoUYkIhSXGAmH"
 TENANT_KEYSTONE_URL = "https://vim.pod4.opnfv.fr:5000/v3"
 
-SOCK_HTTP = "socks5h://127.0.0.1:1080"
+SOCK_HTTP = "socks5h://127.0.0.1:8080"
+
+# PATHS
+TEMPLATES_PATH = os.getcwd().rsplit('/onaptests')[0]+"/templates"
+CONFIGURATION_PATH = os.getcwd().rsplit('/onaptests')[0]+"/src/onaptests/configuration"
