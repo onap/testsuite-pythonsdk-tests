7b3f9abe9764530be8093f2c1d44ae155d0326d3
amine ezziati <aymane.ezziati@gmail.com>
2020-07-24 09:08:42 +0200

    Add CLAMP E2E tests
    Issue-ID: TEST-240
    Signed-off-by: amine ezziati <mohamedamine.ezziati@orange.com>
    Change-Id: Ib612f1670aca10d9ac9f15fadb9cab58a7d8e5a8

---
+++ run_onboard_artifact.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+#
+from onaptests.clamp_scenario import ClampScenario
+import logging
+import logging.config
+
+
+# Configure logging
+logger = logging.getLogger("")
+logger.setLevel(logging.DEBUG)
+logname = "./onap_tests.debug.log"
+fh = logging.FileHandler(logname)
+fh_formatter = logging.Formatter(
+     "%(asctime)s %(levelname)s %(lineno)d:%(filename)s(%(process)d) - %(message)s"
+)
+fh.setFormatter(fh_formatter)
+logger.addHandler(fh)
+
+ubuntu_scenario = ClampScenario(service_name="ubuntu16test")
+service = ubuntu_scenario.onboard_service_artifact()
---
+++ src/onaptests/actions/onboard_clamp.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+# http://www.apache.org/licenses/LICENSE-2.0
+"""Clamp Onboard service class."""
+from onapsdk.sdc.vendor import Vendor
+from onapsdk.sdc.service import Service
+
+from onaptests.configuration import settings
+from onaptests.actions.onboard import Onboard
+
+
+class OnboardClamp(Onboard):
+    """Onboard class to create CLAMP templates."""
+
+    def __init__(self, **kwargs):
+        """Initialize Onboard object."""
+        if "service_name" in kwargs:
+            self.service_name = kwargs['service_name']
+        else:
+            raise ValueError("Service Name to define")
+        self.vf_list = []
+        self.vsp_list = []
+
+    def onboard_artifact(self):
+        """Create service with artifact from scratch."""
+        vendor = Vendor(name=settings.VENDOR_NAME)
+        vendor.onboard()
+        self.onboard_vsp(vendor)
+        # only 1 vf to onboard
+        self.onboard_vf()
+        service = Service(name=self.service_name)
+        service.create()
+        for vf in self.vf_list:
+            service.add_resource(vf)
+        vnf = service.vnfs[0].name
+        payload_file = open(settings.CONFIGURATION_PATH + '/clampnode.yaml', 'rb')
+        data = payload_file.read()
+        service.add_artifact_to_vf(vnf_name=vnf,
+                                   artifact_type="DCAE_INVENTORY_BLUEPRINT",
+                                   artifact_name="clampnode.yaml",
+                                   artifact=data)
+        payload_file.close()
+        service.checkin()
+        service.onboard()
+        return service
