d8bbdbe55ce14f555fb3ab1d0bc71e611ea0932b
amine ezziati <aymane.ezziati@gmail.com>
2020-07-24 09:08:42 +0200

    Add CLAMP E2E tests
    Issue-ID: TEST-240
    Signed-off-by: amine ezziati <mohamedamine.ezziati@orange.com>
    Change-Id: Ib612f1670aca10d9ac9f15fadb9cab58a7d8e5a8

---
+++ src/onaptests/actions/instantiate_loop.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python
+# http://www.apache.org/licenses/LICENSE-2.0
+"""Instantiation class."""
+
+from onapsdk.clamp.loop_instance import LoopInstance
+from onaptests.actions.common import Common
+
+class InstantiateLoop(Common):
+    """class instantiating a closed loop in clamp."""
+
+    def __init__(self, template: str, loop_name: str, operational_policies: list):
+        self.template=template
+        self.loop_name=loop_name
+        self.operational_policies=operational_policies
+        self.set_logger()
+
+    def add_policies(self, loop: LoopInstance) -> None:
+        """add necessary wanted operational policies."""
+        for policy in self.operational_policies:
+            self.logger.info("******** ADD OPERATIONAL POLICY %s *******", policy["name"])
+            added = loop.add_operational_policy(policy_type=policy["policy_type"],
+                                                policy_version=policy["policy_version"])
+            if not added:
+                self.logger.error("an error occured while adding an operational policy")
+            self.logger.info("ADD OPERATION SUCCESSFULY DONE")
+
+    def configure_policies(self, loop: LoopInstance) -> None:
+        """Configure all policies."""
+        self.logger.info("******** UPDATE MICROSERVICE POLICY *******")
+        loop._update_loop_details()
+        loop.update_microservice_policy()
+        for policy in self.operational_policies:
+            #loop.add_op_policy_config(loop.LoopInstance.__dict__[policy["config_function"]])
+            #possible configurations for the moment
+            if policy["name"] == "MinMax":
+                loop.add_op_policy_config(loop.add_minmax_config)
+            if policy["name"] == "Drools":
+                loop.add_op_policy_config(loop.add_drools_conf)
+            if policy["name"] == "FrequencyLimiter":
+                loop.add_op_policy_config(loop.add_frequency_limiter)
+        self.logger.info("Policies are well configured")
+
+    def deploy(self, loop: LoopInstance) -> None:
+        """Deploy closed loop."""
+        self.logger.info("******** SUBMIT POLICIES TO PE *******")
+        submit = loop.act_on_loop_policy(action="submit")
+        self.logger.info("******** CHECK POLICIES SUBMITION *******")
+        if submit :
+            self.logger.info("Policies successfully submited to PE")
+        else:
+            self.logger.error("An error occured while submitting the loop instance")
+            exit(1)
+        self.logger.info("******** DEPLOY LOOP INSTANCE *******")
+        deploy = loop.deploy_microservice_to_dcae()
+        if deploy:
+            self.logger.info("Loop instance %s successfully deployed on DCAE !!", self.loop_name)
+        else:
+            self.logger.error("An error occured while deploying the loop instance")
+            exit(1)
+
+    def instantiate_loop(self):
+        """Instantiate the control loop."""
+        loop = LoopInstance(template=self.template,
+                            name=self.loop_name,
+                            details={})
+        details = loop.create()
+        if details:
+            self.logger.info("Loop instance %s successfully created !!", self.loop_name)
+        else:
+            self.logger.error("An error occured while creating the loop instance")
+
+        self.add_policies(loop=loop)
+        self.configure_policies(loop=loop)
+        self.deploy(loop=loop)
+
+        loop.details = loop._update_loop_details()
+        return loop
---
+++ src/onaptests/clamp_scenario.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python
+#
+# This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+"""Clamp Scenario class."""
+import time
+
+from onapsdk.clamp.clamp_element import Clamp
+from onapsdk.sdc.service import Service
+
+from onaptests.configuration import settings
+from onaptests.actions.onboard_clamp import OnboardClamp
+from onaptests.actions.instantiate_loop import InstantiateLoop
+
+
+class ClampScenario():
+    """class defining the different CLAMP scenarios."""
+
+    count: int = 0
+
+    def __init__(self, **kwargs):
+        """Initialize CLAMP scenario."""
+        if "service_name" in kwargs:
+            self.service_name = kwargs['service_name']
+        else:
+            raise ValueError("Service Name to specify")
+        Service.set_proxy({ 'http': settings.SOCK_HTTP, 'https': settings.SOCK_HTTP})
+        Clamp.set_proxy({ 'http': settings.SOCK_HTTP, 'https': settings.SOCK_HTTP})
+        #must change cert path in python-onapsdk configuration
+        Clamp.create_cert()
+
+    @property
+    def service(self):
+        """Look for the service."""
+        return Service(name=self.service_name)
+
+    def onboard_service_artifact(self):
+        """Create and distribute a service with blueprint artifact."""
+        service = OnboardClamp(service_name=self.service_name)
+        return service.onboard_artifact()
+
+    def check(self, operational_policies: list, is_template: bool = False) -> str:
+        """Check CLAMP requirements to create a loop."""
+        for policy in operational_policies:
+            exist = Clamp.check_policies(policy_name=policy["name"],
+                                         req_policies=30)# 30 required policy
+            if not exist:
+                raise ValueError("Couldn't load the policy %s", policy)
+        if is_template:
+            loop_template = Clamp.check_loop_template(service=self.service)
+            return loop_template
+
+    @staticmethod
+    def instantiate_clamp(loop_template: str, loop_name: str, operational_policies: list):
+        """Instantite a closed loopin CLAMP."""
+        loop = InstantiateLoop(template=loop_template,
+                               loop_name=loop_name,
+                               operational_policies=operational_policies)
+        return loop.instantiate_loop()
+
+    def loop_counter(self, action: str) -> None:
+        """ Count number of loop instances."""
+        if  action == "plus":
+            self.count += 1
+        if  action == "minus":
+            self.count -= 1
+
+    def e2e(self, operational_policies: list, delete: bool = True):
+        """E2E steps"""
+        self.onboard_service_artifact()
+        # Test 1
+        time.sleep(30)
+        # time to wait for template load in CLAMP
+        loop_template = self.check(operational_policies, True)
+        # Test 2
+        loop_name = "instance_" + self.service_name + str(self.count)
+        self.loop_counter(action="plus")
+        loop_instance = self.instantiate_clamp(loop_template=loop_template,
+                                               loop_name=loop_name,
+                                               operational_policies=operational_policies)
+        if delete:
+            self.loop_counter(action="minus")
+            loop_instance.delete()
