4ade64c16d991e326a3597159caea6b1caa0cdbf
amine ezziati <aymane.ezziati@gmail.com>
2020-07-24 09:08:42 +0200

    Add CLAMP E2E tests
    Issue-ID: TEST-240
    Signed-off-by: amine ezziati <mohamedamine.ezziati@orange.com>
    Change-Id: Ib612f1670aca10d9ac9f15fadb9cab58a7d8e5a8

---
+++ run_e2e_cl.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+#
+from onaptests.closed_loop import ClosedLoop
+import logging
+import logging.config
+
+
+logger = logging.getLogger("")
+logger.setLevel(logging.DEBUG)
+logname = "./onap_tests.debug.log"
+fh = logging.FileHandler(logname)
+fh_formatter = logging.Formatter(
+     "%(asctime)s %(levelname)s %(lineno)d:%(filename)s(%(process)d) - %(message)s"
+)
+fh.setFormatter(fh_formatter)
+logger.addHandler(fh)
+
+
+SERVICE_NAME = "my_service"
+
+scenario = ClosedLoop(service_name=SERVICE_NAME) # onboard a a service with 1 vm ubuntu
+scenario.e2e(delete=False)
