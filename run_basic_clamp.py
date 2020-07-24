18642f67fcd7268a914fb6632f0f755336e8ba24
amine ezziati <aymane.ezziati@gmail.com>
2020-07-24 09:08:42 +0200

    Add CLAMP E2E tests
    Issue-ID: TEST-240
    Signed-off-by: amine ezziati <mohamedamine.ezziati@orange.com>
    Change-Id: Ib612f1670aca10d9ac9f15fadb9cab58a7d8e5a8
    Signed-off-by: mrichomme <morgan.richomme@orange.com>

---
+++ run_basic_clamp.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+#
+import logging.config
+from onapsdk.configuration import settings
+from onaptests.steps.loop.clamp import ClampStep
+
+
+if __name__ == "__main__":
+    # logging configuration for onapsdk, it is not requested for onaptests
+    # Correction requested in onapsdk to avoid having this duplicate code
+    logging.config.dictConfig(settings.LOG_CONFIG)
+    logger = logging.getLogger("Instantiate Basic Clamp")
+
+    basic_clamp = ClampStep(
+        cleanup=settings.CLEANUP_FLAG)
+    basic_clamp.execute()
