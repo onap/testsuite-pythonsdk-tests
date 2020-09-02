2010a642d0c643031a7bfc1d42ae021f375ca362
andreasgeissler <andreas-geissler@telekom.de>
2020-09-02 16:09:17 +0200

    Updated onboarding steps and customer creation and added HEAT files Updated README to comply styleguide - hopefully now Issue-ID: TEST-260
    Signed-off-by: andreasgeissler <andreas-geissler@telekom.de>
    Change-Id: I29b9709452ab7cccc1bca54e8e3f8a187c560d14

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import logging.config
 from onapsdk.configuration import settings
-from onaptests.steps.instantiate.service_ala_carte import ServiceAlaCarteInstantiateStep
+from onaptests.steps.instantiate.service_ala_carte import YamlTemplateServiceAlaCarteInstantiateStep
 
 
 
@@ -9,5 +9,6 @@ if __name__ == "__main__":
     # Correction requested in onapsdk to avoid having this duplicate code
     logging.config.dictConfig(settings.LOG_CONFIG)
 
-    service_inst = ServiceAlaCarteInstantiateStep()
+    service_inst = YamlTemplateServiceAlaCarteInstantiateStep()
+    #service_inst = ServiceAlaCarteInstantiateStep()
     service_inst.execute()
