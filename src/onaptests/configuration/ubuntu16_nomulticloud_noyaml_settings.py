cd7923ff1937a20c6e0f80053000d299b1be2ab5
mrichomme <morgan.richomme@orange.com>
2020-09-08 11:43:20 +0200

    Use clouds.yaml rather than duplicating the openstack data in the conf
    Issue-ID: TEST-261
    
    Signed-off-by: mrichomme <morgan.richomme@orange.com>
    Change-Id: I624fca14a30dde9e704a9b982b55cbb8028b37fd
    Signed-off-by: mrichomme <morgan.richomme@orange.com>

--- src/onaptests/configuration/ubuntu16_nomulticloud_noyaml_settings.py
+++ src/onaptests/configuration/ubuntu16_nomulticloud_noyaml_settings.py
@@ -1,9 +1,11 @@
 import os
 import openstack
 
-# pylint: disable=unused-import
-from .settings import *
+from .settings import * # pylint: disable=W0614
 
+""" Specific ubuntu16 without multicloud and without yaml config scenario."""
+
+# pylint: disable=bad-whitespace
 # The ONAP part
 USE_MULTICLOUD = False
 
@@ -13,10 +15,8 @@ SERVICE_NAME = "basicvm-ubuntu-service"
 VF_NAME = "basicvm_ubuntu_vf"
 
 CLOUD_REGION_CLOUD_OWNER = "basicvm-cloud-owner"
-CLOUD_REGION_ID = "RegionOne"
 CLOUD_REGION_TYPE = "openstack"
 CLOUD_REGION_VERSION = "openstack"
-CLOUD_DOMAIN = "Default"
 
 AVAILABILITY_ZONE_NAME = "basicvm-availability-zone"
 AVAILABILITY_ZONE_TYPE = "nova"
@@ -35,22 +35,29 @@ SERVICE_INSTANCE_NAME = "basicvm_ubuntu16_service_instance"
 VSP_FILE_PATH = "templates/heat_files/ubuntu16/ubuntu16.zip"
 
 # The cloud Part
-# TODO use the openstack client and assume a cloud.yaml is Provided
-# to avoid data duplication
-CLOUD_REGION_ID = "RegionOne"
-
-TEST_CLOUD = os.getenv('OS_TEST_CLOUD', 'onap-cloud-config')
+# Assuming a cloud.yaml is available, use the openstack client
+# to retrieve cloud info and avoid data duplication
+TEST_CLOUD = os.getenv('OS_TEST_CLOUD')
 try:
-    cloud = openstack.connect(cloud=TEST_CLOUD)
-    VIM_USERNAME = cloud.config.auth['username']
-    VIM_PASSWORD = cloud.config.auth['password']
-    VIM_SERVICE_URL = cloud.config.auth['auth_url']
-    # need a keystone authent to retrieve project info
-    TENANT_ID = "" # Fill me
-    TENANT_NAME = "" # Fill me
-except ValueError:
+    if TEST_CLOUD is not None:
+        cloud = openstack.connect(cloud=TEST_CLOUD)
+        VIM_USERNAME = cloud.config.auth['username']
+        VIM_PASSWORD = cloud.config.auth['password']
+        VIM_SERVICE_URL = cloud.config.auth['auth_url']
+        TENANT_ID = cloud.config.auth['project_id']
+        TENANT_NAME = cloud.config.auth['project_name']
+        CLOUD_REGION_ID = cloud.config.region_name
+        CLOUD_DOMAIN = cloud.config.auth['project_domain_name']
+    else:
+        raise KeyError
+except KeyError:
+    # If you do not use the cloud.yaml as imput for your openstack
+    # put the input data here
+    # Note if 1 parameter is missing in the clouds.yaml, we fallback here
     TENANT_ID = "" # Fill me
     TENANT_NAME = "" # Fill me
     VIM_USERNAME = ""  # Fill me
     VIM_PASSWORD = ""  # Fill me
     VIM_SERVICE_URL = ""  # Fill me
+    CLOUD_REGION_ID = "RegionOne" # Update me if needed
+    CLOUD_DOMAIN = "Default" # Update me if needed
