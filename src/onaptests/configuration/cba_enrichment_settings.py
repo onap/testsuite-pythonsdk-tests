9ec2cd09a4f7db7b992f34137d7bc340fee74047
Michal Jagiello <michal.jagiello@t-mobile.pl>
2020-11-18 11:20:59 +0000

    CDS onboarding steps and simple CBA enrichment scenarion.
    Steps for data dictionary upload and CBA enrichment
    
    Issue-ID: TEST-245
    Signed-off-by: Michal Jagiello <michal.jagiello@t-mobile.pl>
    Change-Id: If7f3346c79ae97e35b21e919435270a0c54f77dd

---
+++ src/onaptests/configuration/cba_enrichment_settings.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+from .settings import *
+
+SERVICE_NAME = "CDS blueprint enrichment"
+
+CLEANUP_FLAG = True
+
+CDS_DD_FILE = Path(Path(__file__).parent.parent, "templates/artifacts/dd.json")
+CDS_CBA_UNENRICHED = Path(Path(__file__).parent.parent, "templates/artifacts/PNF_DEMO.zip")
+CDS_CBA_ENRICHED = "/tmp/PNF_DEMO_enriched.zip"
