3165bc2a5dcd476fcac858c85cc6f3e8f7253ccc
mrichomme <morgan.richomme@orange.com>
2020-12-04 14:34:29 +0100

    basic_cds adapatations for xtesting
    Issue-ID: TEST-245
    
    Signed-off-by: mrichomme <morgan.richomme@orange.com>
    Change-Id: I4bb0a1c728ad8b982e6936c8a3bc5572ee08a6b4

--- src/onaptests/scenario/cds_blueprint_enrichment.py
+++ src/onaptests/scenario/cds_blueprint_enrichment.py
@@ -2,10 +2,12 @@
 """Simple CDS blueprint erichment test scenario."""
 
 import logging
+import time
 
 from onapsdk.configuration import settings
 from xtesting.core import testcase
 
+import onaptests.utils.exceptions as onap_test_exceptions
 from onaptests.steps.onboard.cds import CbaEnrichStep
 
 
@@ -14,16 +16,24 @@ class CDSBlueprintEnrichment(testcase.TestCase):
 
     __logger = logging.getLogger(__name__)
 
-    def __init__(self):
+    def __init__(self, **kwargs):
         """Init CDS blueprint enrichment use case."""
+        if "case_name" not in kwargs:
+            kwargs["case_name"] = 'basic_cds'
+        super(CDSBlueprintEnrichment, self).__init__(**kwargs)
         self.__logger.debug("CDS blueprint enrichment initialization")
-        super.__init__()
         self.test = CbaEnrichStep(
                 cleanup=settings.CLEANUP_FLAG)
+        self.start_time = None
+        self.stop_time = None
+        self.result = 0
 
     def run(self):
         self.__logger.debug("CDS blueprint enrichment run")
+        self.start_time = time.time()
         self.test.execute()
+        self.result = 100
+        self.stop_time = time.time()
 
     def clean(self):
         """Clean Additional resources if needed."""
