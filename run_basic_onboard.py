89d60c4e7485205659c7514cf15adea9cd32832d
Chereau Natacha <natacha.chereau@orange.com>
2021-01-13 10:54:46 +0100

    [PythonSDK-tests] Add basic_onboard testcase
    Issue-ID: TEST-288
    
    Signed-off-by: Chereau Natacha <natacha.chereau@orange.com>
    Change-Id: Ide7267428b5ca694dc3ca44a4c81730233610b78

---
+++ run_basic_onboard.py
@@ -0,0 +1,21 @@
+import logging.config
+import time
+import onaptests.utils.exceptions as onap_test_exceptions
+from onapsdk.configuration import settings
+from onaptests.steps.onboard.service import YamlTemplateServiceOnboardStep
+
+
+
+if __name__ == "__main__":
+    # logging configuration for onapsdk, it is not requested for onaptests
+    # Correction requested in onapsdk to avoid having this duplicate code
+    logging.config.dictConfig(settings.LOG_CONFIG)
+    logger = logging.getLogger("Basic Onboard")
+
+    basic_vm_onboard = YamlTemplateServiceOnboardStep(
+        cleanup=settings.CLEANUP_FLAG)
+    try:
+        basic_vm_onboard.execute()
+    except onap_test_exceptions.TestConfigurationException:
+        logger.error("Basic Onboard configuration error")
+    basic_vm_onboard.reports_collection.generate_report()
