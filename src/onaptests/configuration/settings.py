485474b7b92e92dc2a4d936455d774a203b5862a
Chereau Natacha <natacha.chereau@orange.com>
2021-01-13 10:54:46 +0100

    [PythonSDK-tests] Add basic_onboard testcase
    Issue-ID: TEST-288
    
    Signed-off-by: Chereau Natacha <natacha.chereau@orange.com>
    Change-Id: Ide7267428b5ca694dc3ca44a4c81730233610b78
    (cherry picked from commit ff24de49ef658ffe0aa13d494e8201bd181bce2c)

--- src/onaptests/configuration/settings.py
+++ src/onaptests/configuration/settings.py
@@ -43,4 +43,8 @@ REPORTING_FILE_PATH = "/tmp/reporting.html"
 K8S_REGION_TYPE = "k8s"
 K8S_CONFIG = None  # None means it will use default config (~/.kube/config)
 K8S_NAMESPACE = "onap"  # Kubernetes namespace
+<<<<<<< HEAD   (fcb7ff Merge "Nodeport cleanup in basic_cds test" into guilin)
 # SOCK_HTTP = "socks5h://127.0.0.1:8080"
+=======
+#SOCK_HTTP = "socks5h://127.0.0.1:8091"
+>>>>>>> CHANGE (ff24de [PythonSDK-tests] Add basic_onboard testcase)
