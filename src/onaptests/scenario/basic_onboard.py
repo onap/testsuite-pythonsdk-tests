272c451228350247e07a1165535b3b8fc0800f7c
mrichomme <morgan.richomme@orange.com>
2021-01-18 15:26:53 +0100

    [TESTCASE] fix basic_onboard scenario
    the run_basic_onboard.py is working, but the scenario used by xtesting
    cannot work properly.
    
    Tested on Daily master lab.
    
    Issue-ID: TEST-288
    
    Signed-off-by: mrichomme <morgan.richomme@orange.com>
    Change-Id: Ibc5482607a738692a05260913eeb322a0933ff03
    (cherry picked from commit d6ab274b76e48164d3aefe702cf8ea2f87c98004)

---
+++ src/onaptests/scenario/basic_onboard.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+"""Basic Onboard test case."""
+import logging
+import time
+from xtesting.core import testcase
+from onapsdk.configuration import settings
+from onapsdk.exceptions import SDKException
+from onaptests.steps.onboard.service import YamlTemplateServiceOnboardStep
+from onaptests.utils.exceptions import OnapTestException
+
+class BasicOnboard(testcase.TestCase):
+    """Onboard a simple VM with ONAP."""
+
+    __logger = logging.getLogger(__name__)
+
+    def __init__(self, **kwargs):
+        """Init BasicOnboard."""
+        # import basic_onboard_settings needed
+        if "case_name" not in kwargs:
+            kwargs["case_name"] = 'basic_onboard'
+        super(BasicOnboard, self).__init__(**kwargs)
+        self.__logger.debug("BasicOnboard init started")
+        self.test = YamlTemplateServiceOnboardStep(
+            cleanup=settings.CLEANUP_FLAG)
+        self.start_time = None
+        self.stop_time = None
+        self.result = 0
+
+    def run(self):
+        """Run basic_onboard and onboard a simple service"""
+        self.start_time = time.time()
+        self.__logger.debug("start time")
+        try:
+            self.test.execute()
+            self.__logger.info("VNF basic_vm successfully onboarded")
+            self.result = 100
+        except OnapTestException as exc:
+            self.result = 0
+            self.__logger.error(exc.error_message)
+        except SDKException:
+            self.result = 0
+            self.__logger.error("SDK Exception")
+        finally:
+            self.stop_time = time.time()
+
+    def clean(self):
+        """Clean Additional resources if needed."""
+        self.__logger.info("Generate Test report")
+        self.test.reports_collection.generate_report()
