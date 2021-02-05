cc4a4b310ea0446e7d99cf817a6e7d950b984a48
Michal Jagiello <michal.jagiello@t-mobile.pl>
2021-02-05 20:37:02 +0000

    Python SDK 7.4.0 compatibility
    Use Python SDK exceptions.
    
    Issue-ID: TEST-302
    Signed-off-by: Michal Jagiello <michal.jagiello@t-mobile.pl>
    Change-Id: I76996817c0d7b91c0214ad521c3768682ff07e83

--- src/onaptests/steps/cloud/register_cloud.py
+++ src/onaptests/steps/cloud/register_cloud.py
@@ -3,6 +3,7 @@ from uuid import uuid4
 
 from onapsdk.aai.cloud_infrastructure import CloudRegion
 from onapsdk.configuration import settings
+from onapsdk.exceptions import ResourceNotFound
 
 from ..base import BaseStep
 
@@ -43,7 +44,7 @@ class RegisterCloudRegionStep(BaseStep):
                 cloud_owner=settings.CLOUD_REGION_CLOUD_OWNER,
                 cloud_region_id=settings.CLOUD_REGION_ID,
             )
-        except ValueError:
+        except ResourceNotFound:
             self._logger.info("*Create the cloud region *")
             cloud_region: CloudRegion = CloudRegion.create(
                 cloud_owner=settings.CLOUD_REGION_CLOUD_OWNER,
@@ -83,7 +84,7 @@ class RegisterCloudRegionStep(BaseStep):
             # if it does not exist, create it
             try:
                 cloud_region.get_tenant(settings.TENANT_ID)
-            except ValueError:
+            except ResourceNotFound:
                 self._logger.warning("Impossible to retrieve the Specificed Tenant")
                 self._logger.debug("If no multicloud selected, add the tenant")
                 cloud_region.add_tenant(
@@ -95,7 +96,7 @@ class RegisterCloudRegionStep(BaseStep):
             try:
                 cloud_region.get_availability_zone_by_name(
                     settings.AVAILABILITY_ZONE_NAME)
-            except ValueError:
+            except ResourceNotFound:
                 cloud_region.add_availability_zone(
                     settings.AVAILABILITY_ZONE_NAME,
                     settings.AVAILABILITY_ZONE_TYPE)
