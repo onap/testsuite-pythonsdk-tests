eee0243ad8762effa20af9c533d9b413612fe91d
Michal Jagiello <michal.jagiello@t-mobile.pl>
2021-03-23 18:23:48 +0000

    Basic VM macro
    Issue-ID: INT-1894
    Signed-off-by: Michal Jagiello <michal.jagiello@t-mobile.pl>
    Change-Id: Idd977cf3082587746fe27718a284197fda4afe5c
    Signed-off-by: morganrol <morgan.richomme@orange.com>

--- src/onaptests/steps/onboard/vf.py
+++ src/onaptests/steps/onboard/vf.py
@@ -85,4 +85,15 @@ class YamlTemplateVfOnboardStep(YamlTemplateBaseStep):
             for vnf in self.yaml_template["vnfs"]:
                 vsp: Vsp = Vsp(name=f"{vnf['vnf_name']}_VSP")
                 vf: Vf = Vf(name=vnf['vnf_name'], vsp=vsp)
+                if all([x in vnf for x in ["vnf_artifact_type",
+                                           "vnf_artifact_name",
+                                           "vnf_artifact_label",
+                                           "vnf_artifact_file_path"]]):
+                    vf.create()
+                    vf.add_deployment_artifact(
+                        artifact_type=vnf["vnf_artifact_type"],
+                        artifact_name=vnf["vnf_artifact_name"],
+                        artifact_label=vnf["vnf_artifact_label"],
+                        artifact=vnf["vnf_artifact_file_path"]
+                    )
                 vf.onboard()
