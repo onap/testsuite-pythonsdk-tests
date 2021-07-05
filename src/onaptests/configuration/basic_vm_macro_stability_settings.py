7fbd9f67fa43997bc8b030ab24dcc4315493c1b1
Michal Jagiello <michal.jagiello@t-mobile.pl>
2021-07-05 10:45:05 +0000

    [TEST] Basic macro stability scenario
    Issue-ID: TEST-349
    Signed-off-by: Michal Jagiello <michal.jagiello@t-mobile.pl>
    Change-Id: I951e9720668de9bf00136d4e043c9fe8950061f1

---
+++ src/onaptests/configuration/basic_vm_macro_stability_settings.py
@@ -0,0 +1,4 @@
+from .basic_vm_macro_settings import *   # pylint: disable=W0614
+
+SERVICE_YAML_TEMPLATE = Path(Path(__file__).parent.parent, "templates/vnf-services/" +
+                         "basic_vm_macro_stability-service.yaml")
