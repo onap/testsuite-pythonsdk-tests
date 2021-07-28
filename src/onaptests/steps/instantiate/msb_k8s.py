3bdfe4cfb7a4c08bbca048120a73397dbdf17c4c
Michal Jagiello <michal.jagiello@t-mobile.pl>
2021-07-28 11:20:49 +0000

    [TEST] Additional cleanup before MSB K8S instance creation
    Issue-ID: TEST-354
    Signed-off-by: Michal Jagiello <michal.jagiello@t-mobile.pl>
    Change-Id: I9287e1201bf8beeb6c528e41246e08559c852366

--- src/onaptests/steps/instantiate/msb_k8s.py
+++ src/onaptests/steps/instantiate/msb_k8s.py
@@ -1,9 +1,44 @@
 """MSB k8s instantiation module."""
+import time
+
 from onapsdk.configuration import settings
+from onapsdk.exceptions import APIError
 from onapsdk.msb.k8s import Instance
 
 from onaptests.steps.base import BaseStep
 from onaptests.steps.onboard.msb_k8s import CreateProfileStep
+
+
+class InstancesCleanup(BaseStep):
+    """Delete old instances which were not cleaned up properly."""
+
+    @property
+    def description(self) -> str:
+        """Step description."""
+        return ("Delete old instances which were created using same MSB_K8S_RESOURCE_NAME_PREFIX"
+                " and were not cleaned up.")
+
+    @property
+    def component(self) -> str:
+        """Component name."""
+        return "K8S plugin"
+
+    @BaseStep.store_state
+    def execute(self) -> None:
+        """Create instance using MSB K8S plugin."""
+        super().execute()
+        self._logger.debug("Delete all instances which are created using definition with same prefix ")
+        any_deleted: bool = False
+        try:
+            for instance in Instance.get_all():
+                if instance.request.profile_name.startswith(settings.MSB_K8S_RESOURCE_NAME_PREFIX):
+                    self._logger.debug("Delete %s instance", instance.instance_id)
+                    instance.delete()
+                    any_deleted = True
+            if any_deleted:
+                time.sleep(settings.MSB_K8S_CLEANUP_WAIT_TIME)  # Give it some time to delete k8s resources (pods, services, deployments...)
+        except APIError:
+            self._logger.debug("There are no MSB K8S instances")
 
 
 class CreateInstanceStep(BaseStep):
@@ -16,6 +51,7 @@ class CreateInstanceStep(BaseStep):
             - CreateProfileStep.
         """
         super().__init__(cleanup=cleanup)
+        self.add_step(InstancesCleanup())
         self.add_step(CreateProfileStep(cleanup=cleanup))
         self.instance: Instance = None
 
