c3ef82f489b915095b7464fb119daf43b84f67f8
Julien Fontaine <julien.fontaine@bell.ca>
2021-08-04 15:52:02 -0400

    [TEST] Added support for multi-vnf macro instantiation
    Decoupled service YAML template into a model YAML template and a (SO) service YAML template.
    Model YAML template will be used during the onboarding steps and service YAML template will be used to generate payload when sending instantiation request to SO.
    Service YAML template reference model name to use for its VNF/VF-Modules using "model_name" field.
    This provide more flexibility to design the testcase and enables to setup more complex testcases like instantiating several VNF/VF-MOdules using the same SDC model infos.
    This patch aims to provide backward compatibility for existing testcases based on YAML template.
    
    Issue-ID: TEST-358
    Signed-off-by: Julien Fontaine <julien.fontaine@bell.ca>
    Change-Id: I69d370eff4d383d5af135206476c65e4a56e4ee5

--- src/onaptests/steps/instantiate/vl_ala_carte.py
+++ src/onaptests/steps/instantiate/vl_ala_carte.py
@@ -56,6 +56,10 @@ class YamlTemplateVlAlaCarteInstantiateStep(YamlTemplateBaseStep):
         return self.parent.yaml_template
 
     @property
+    def model_yaml_template(self) -> dict:
+        return {}
+
+    @property
     def service_name(self) -> str:
         """Service name.
 
