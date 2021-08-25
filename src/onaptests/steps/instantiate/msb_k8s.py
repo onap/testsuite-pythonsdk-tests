e25da3ffce5fd4c3c8502dd2843b92eaea4af2af
Morgan Richomme <morgan.richomme@orange.com>
2021-08-25 14:12:42 +0000

    Revert "[TEST] Additional cleanup before MSB K8S instance creation"
    This reverts commit 0e2af06b54eaf80c48b71e88afce16d5a1117a7a.
    
    Reason for revert: <regression in gating and daily>
    
    Issue-ID: TEST-354
    Change-Id: I1592b4b4af590509e5cca0e90f2519ef5c8a0362
    Signed-off-by: mrichomme <morgan.richomme@orange.com>

--- src/onaptests/steps/instantiate/msb_k8s.py
+++ src/onaptests/steps/instantiate/msb_k8s.py
@@ -1,40 +1,9 @@
 """MSB k8s instantiation module."""
-import time
-
 from onapsdk.configuration import settings
 from onapsdk.msb.k8s import Instance
 
 from onaptests.steps.base import BaseStep
 from onaptests.steps.onboard.msb_k8s import CreateProfileStep
-
-
-class InstancesCleanup(BaseStep):
-    """Delete old instances which were not cleaned up properly."""
-
-    @property
-    def description(self) -> str:
-        """Step description."""
-        return ("Delete old instances which were created using same MSB_K8S_RESOURCE_NAME_PREFIX"
-                " and were not cleaned up.")
-
-    @property
-    def component(self) -> str:
-        """Component name."""
-        return "K8S plugin"
-
-    @BaseStep.store_state
-    def execute(self) -> None:
-        """Create instance using MSB K8S plugin."""
-        super().execute()
-        self._logger.debug("Delete all instances which are created using definition with same prefix ")
-        any_deleted: bool = False
-        for instance in Instance.get_all():
-            if instance.request.profile_name.startswith(settings.MSB_K8S_RESOURCE_NAME_PREFIX):
-                self._logger.debug("Delete %s instance", instance.instance_id)
-                instance.delete()
-                any_deleted = True
-        if any_deleted:
-            time.sleep(settings.MSB_K8S_CLEANUP_WAIT_TIME)  # Give it some time to delete k8s resources (pods, services, deployments...)
 
 
 class CreateInstanceStep(BaseStep):
@@ -47,7 +16,6 @@ class CreateInstanceStep(BaseStep):
             - CreateProfileStep.
         """
         super().__init__(cleanup=cleanup)
-        self.add_step(InstancesCleanup())
         self.add_step(CreateProfileStep(cleanup=cleanup))
         self.instance: Instance = None
 
@@ -66,9 +34,9 @@ class CreateInstanceStep(BaseStep):
         """Create instance using MSB K8S plugin."""
         super().execute()
         self.instance = Instance.create(cloud_region_id=settings.CLOUD_REGION_ID,
-                                        profile_name=settings.MSB_K8S_PROFILE_NAME,
-                                        rb_name=settings.MSB_K8S_RB_NAME,
-                                        rb_version=settings.MSB_K8S_RB_VERSION)
+                                        profile_name=settings.PNF_PROFILE_NAME,
+                                        rb_name=settings.PNF_RB_NAME,
+                                        rb_version=settings.PNF_RB_VERSION)
 
     @BaseStep.store_state(cleanup=True)
     def cleanup(self) -> None:
