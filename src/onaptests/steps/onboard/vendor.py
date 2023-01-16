26f4eabbeb2e35cba64a9a848c97124663293f56
Michal Jagiello <michal.jagiello@t-mobile.pl>
2023-01-16 15:17:01 +0000

    Add cleanup to sdc resources ondboarding step
    Delete SDC resources on cleanup
    VSP is going to be deleted only for basic_onboard step as it's failing
    
    Issue-ID: INT-2193
    Signed-off-by: Michal Jagiello <michal.jagiello@t-mobile.pl>
    Change-Id: Id9e51f07a9c91a02b67a87eb897a2a4d19fa6738

--- src/onaptests/steps/onboard/vendor.py
+++ src/onaptests/steps/onboard/vendor.py
@@ -28,3 +28,10 @@ class VendorOnboardStep(BaseStep):
         super().execute()
         vendor: Vendor = Vendor(name=settings.VENDOR_NAME)
         vendor.onboard()
+
+    @BaseStep.store_state(cleanup=True)
+    def cleanup(self) -> None:
+        if settings.SDC_CLEANUP:
+            vendor: Vendor = Vendor(name=settings.VENDOR_NAME)
+            vendor.delete()
+        super().cleanup()
