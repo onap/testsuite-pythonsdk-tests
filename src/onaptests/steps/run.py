f176e48e7188c5f3ca3291e2ab86ac29896e12e6
pawel.denst <pawel.denst@external.t-mobile.pl>
2023-05-10 10:50:58 +0000

    ONAP Python SDK tests should generate JSON artifact (next to HTML)
    Added new method responsible for generating json file report, also added run.py to try to run it (may be deleted after) and created test for it
    
    Issue-ID: INT-2235
    Signed-off-by: pawel.denst <pawel.denst@external.t-mobile.pl>
    Change-Id: I896d5eb1b71336ebb211501ae51c612538c60deb

---
+++ src/onaptests/steps/run.py
@@ -0,0 +1,18 @@
+import json
+import os
+from onapsdk.configuration import settings
+from onaptests.steps.reports_collection import ReportsCollection, Report, ReportStepStatus
+
+settings.SERVICE_NAME = 'service'
+settings.SERVICE_DETAILS = 'detailsv4'
+settings.REPORTING_FILE_PATH = os.path.join(os.getcwd(), 'reports2.json') 
+
+reports = ReportsCollection()
+
+reports.put(Report("Step 1", ReportStepStatus.PASS, 10.0))
+reports.put(Report("Step 2", ReportStepStatus.FAIL, 20.0))
+reports.put(Report("Step 3", ReportStepStatus.NOT_EXECUTED, 0.0))
+
+report_dict = reports.generate_report_json()
+
+print(report_dict)
