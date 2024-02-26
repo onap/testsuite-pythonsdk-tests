2417fe1a60ebf8aa05be932a421cb09e71cd1783
Lukasz Rajewski <lukasz.rajewski@t-mobile.pl>
2024-02-26 09:21:36 +0100

    Fix the issue of basic_vm_macro configuration
    - Fix the issue of basic_vm_macro introduced after refactoring
    - new SDK version used
    - tox updated with helper commands
    
    Issue-ID: TEST-402
    Signed-off-by: Lukasz Rajewski <lukasz.rajewski@t-mobile.pl>
    Change-Id: I46aedcd08279142b2da48a431ab4f21ae1c9d3d1

--- src/onaptests/configuration/basic_vm_macro_settings.py
+++ src/onaptests/configuration/basic_vm_macro_settings.py
@@ -33,16 +33,16 @@ COMPLEX_DATA_CENTER_CODE = "1234-5"
 
 GLOBAL_CUSTOMER_ID = "basicvm-customer"
 
-# if not IF_VALIDATION:
-#     TEST_CLOUD = os.getenv('OS_TEST_CLOUD')
-#     cloud = openstack.connect(cloud=TEST_CLOUD)
-#     VIM_USERNAME = cloud.config.auth.get('username', 'Fill me')
-#     VIM_PASSWORD = cloud.config.auth.get('password', 'Fill me')
-#     VIM_SERVICE_URL = cloud.config.auth.get('auth_url', 'Fill me')
-#     TENANT_ID = cloud.config.auth.get('project_id', 'Fill me')
-#     TENANT_NAME = cloud.config.auth.get('project_name', 'Fill me')
-#     CLOUD_REGION_ID = cloud.config.auth.get('region_name', 'RegionOne')
-#     CLOUD_DOMAIN = cloud.config.auth.get('project_domain_name', 'Default')
+if not IF_VALIDATION:
+    TEST_CLOUD = os.getenv('OS_TEST_CLOUD')
+    cloud = openstack.connect(cloud=TEST_CLOUD)
+    VIM_USERNAME = cloud.config.auth.get('username', 'Fill me')
+    VIM_PASSWORD = cloud.config.auth.get('password', 'Fill me')
+    VIM_SERVICE_URL = cloud.config.auth.get('auth_url', 'Fill me')
+    TENANT_ID = cloud.config.auth.get('project_id', 'Fill me')
+    TENANT_NAME = cloud.config.auth.get('project_name', 'Fill me')
+    CLOUD_REGION_ID = cloud.config.auth.get('region_name', 'RegionOne')
+    CLOUD_DOMAIN = cloud.config.auth.get('project_domain_name', 'Default')
 
 OWNING_ENTITY = "basicvm-oe"
 PROJECT = "basicvm-project"
